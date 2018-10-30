f8170850e1f279ab9fdf2b76489418c3f6a42344
Mohammed Islam <moeislamf@gmail.com>
2018-10-30 00:06:13 -0400

    afdsfd

--- src/main/java/coreClasses/BaseTestScript.java
+++ src/main/java/coreClasses/BaseTestScript.java
@@ -1,21 +1,42 @@
 package coreClasses;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class BaseTestScript {
 	public static WebDriver driver;
 	static String APPLICATION_URL;
 
+	 protected static ExtentReports extent;
+	 protected ExtentTest logger;
+	
 	@BeforeTest
 	public static void beoferTest() throws IOException {
 
 		driver = setUp();
+		
+		  extent = new ExtentReports (System.getProperty("user.dir") +"/test-output/STMExtentReport.html", true);
+		  //extent.addSystemInfo("Environment","Environment Name")
+		  extent
+		                .addSystemInfo("Host Name", "SoftwareTestingMaterial")
+		                .addSystemInfo("Environment", "Automation Testing")
+		                .addSystemInfo("User Name", "Rajkumar SM");
+		                //loading the external xml file (i.e., extent-config.xml) which was placed under the base directory
+		                //You could find the xml file below. Create xml file in your project and copy past the code mentioned below
+		                extent.loadConfig(new File(System.getProperty("user.dir")+"\\extent-config.xml"));
+		 
 		// APPLICATION_URL =
 		// textingProperties.getInstance().getProperty("qa_DEV_URL").trim();
 		driver.navigate().to("https://test.textingpro.com/index/index");
@@ -30,7 +51,21 @@ public class BaseTestScript {
 		return new ChromeDriver(options);
 
 	}
+	
 
+	@AfterMethod
+	 public void getResult(ITestResult result){
+	  if(result.getStatus() == ITestResult.FAILURE){
+	   logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getName());
+	   logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getThrowable());
+	  }else if(result.getStatus() == ITestResult.SKIP){
+	   logger.log(LogStatus.SKIP, "Test Case Skipped is "+result.getName());
+	  }
+	  // ending test
+	  //endTest(logger) : It ends the current test and prepares to create HTML report
+	  extent.endTest(logger);
+	 }
+	
 	// @BeforeMethod
 	// protected void beforeMethod() throws InterruptedException, IOException {
 	// driver.get(APPLICATION_URL);
@@ -41,9 +76,24 @@ public class BaseTestScript {
 
 	@AfterTest
 	public void tearDown() {
-	//	driver.close();
-	//	driver.quit();
+	
 
+		  // writing everything to document
+		  //flush() - to write or update test information to your report. 
+		                extent.flush();
+		                //Call close() at the very end of your session to clear all resources. 
+		                //If any of your test ended abruptly causing any side-affects (not all logs sent to ExtentReports, information missing), this method will ensure that the test is still appended to the report with a warning message.
+		                //You should call close() only once, at the very end (in @AfterSuite for example) as it closes the underlying stream. 
+		                //Once this method is called, calling any Extent method will throw an error.
+		                //close() - To close all the operation
+		                extent.close();
+		                driver.close();
+		            	driver.quit();
+		    }
+		 
+		
+	
+	
 	}
 
-}
+
--- src/test/java/register_functionality/createNewKeyword.java
+++ src/test/java/register_functionality/createNewKeyword.java
@@ -5,43 +5,52 @@ import java.io.IOException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.LogStatus;
+
 import coreClasses.BaseTestScript;
 import pageObjects.addKeyword;
 import pageObjects.keywordPage;
 import pageObjects.loginPage;
 import pageObjects.userDashboardPage;
 
-public class createNewKeyword extends BaseTestScript{
-	
+public class createNewKeyword extends BaseTestScript {
+
 	loginPage login;
 	userDashboardPage keyword;
 	keywordPage createNewKeyword;
 	addKeyword createkeyword;
-	
+
 	@Test
 	public void createkeyword() throws IOException, InterruptedException {
-		
-	login = new loginPage(BaseTestScript.driver);
-	login.doLogin("textingpro3@gmail.com", "password");
-	
-	keyword= new userDashboardPage(BaseTestScript.driver);
-	keyword.keywordMenu();
-	
-	createNewKeyword = new keywordPage(BaseTestScript.driver);
-	createNewKeyword.clickCreateNewKeywordButton();
-	
-	createkeyword = new addKeyword(BaseTestScript.driver);
-	createkeyword.addKeywordElements();
-	
-	if(createNewKeyword.keywordConfirmation()) {
-		System.out.println("Keyword successfully created");
-	} else {
-		System.out.println("Keyword wasn't created");
-		Assert.assertTrue(false);
+
+		login = new loginPage(BaseTestScript.driver);
+		login.doLogin("textingpro3@gmail.com", "password");
+
+		keyword = new userDashboardPage(BaseTestScript.driver);
+		keyword.keywordMenu();
+
+		createNewKeyword = new keywordPage(BaseTestScript.driver);
+		createNewKeyword.clickCreateNewKeywordButton();
+
+		createkeyword = new addKeyword(BaseTestScript.driver);
+		createkeyword.addKeywordElements();
+
+		if (createNewKeyword.keywordConfirmation()) {
+			System.out.println("Keyword successfully created");
+
+			logger = extent.startTest("passTest");
+			Assert.assertTrue(true);
+			// To generate the log when the test case is passed
+			logger.log(LogStatus.PASS, "Test Case Passed is passTest");
+
+		} else {
+			System.out.println("Keyword wasn't created");
+
+			logger = extent.startTest("failTest");
+			Assert.assertTrue(false);
+			logger.log(LogStatus.PASS, "Test Case (failTest) Status is passed");
+		}
+
 	}
-		
-	}
-	
-	
 
 }
